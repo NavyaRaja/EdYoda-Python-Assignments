ede636e1e0ff41a4d4d7bca55e27dc9146a74c37
NavyaRaja <110919834+NavyaRaja@users.noreply.github.com>
2022-09-29 21:46:53 +0530

    Add files via upload

---
+++ Pair of Integers.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def getPairsCount(arr, n, sum):
+ 
+    count = 0 
+    
+    
+    for i in range(0, n):
+        for j in range(i + 1, n):
+            if arr[i] + arr[j] == sum:
+                count += 1
+ 
+    return count
+arr = [1, 5, 7, -1, 5]
+n = len(arr)
+k = 6
+ 
+
+print("Count of pairs is", getPairsCount(arr, n, k))
+ 
+
+
+# In[ ]:
+
+
+
+
